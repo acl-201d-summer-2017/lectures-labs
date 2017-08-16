d55f2455bd29d2bf132679195a00ab30f9887dc2
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-08-16 08:06:40 -0700

    update c13

--- class-13-local_storage-ui_ux/README.md
+++ class-13-local_storage-ui_ux/README.md
@@ -31,7 +31,9 @@
         * remember those ol' alerts? [sweet alert](http://www.javascripting.com/view/sweetalert)
         * [5 examples we can learn from](https://www.interaction-design.org/literature/article/bad-design-vs-good-design-5-examples-we-can-learn-frombad-design-vs-good-design-5-examples-we-can-learn-from-130706)
     * user experience
-        * how the interactions affect the user's perception and experience
+        * how the interactions and more affect the user's perception and experience
+        * goal of ux:
+            * influence user's perception of brand/product
         * encompasses many, many things:
             - onboarding
             - branding
@@ -40,11 +42,32 @@
             - and more!
         * [user onboarding teardowns](https://www.useronboard.com/onboarding-teardowns/)
         * [badux](http://badux.tumblr.com/)
+    * common practices you've seen in the wild
+        - hamburger menu
+            - pro: hides menu
+            - con: hides menu, also not 100% intuitive
+            - [example](https://theoutline.com/)
+        - # of items in navigation
+            - keep it between 5-7
+            - [example](https://wordpress.com/)
+            - [example - too many options](https://www.usatoday.com/)
+        - links should look like links, or at least different from standard text
+            - otherwise, why would you click?
+            - [example](http://www.goodui.org/)
+        - email sign up pop ups
+          - show on switching to tab
+          - show as little pop up on bottom
+        - logging in with social media
+        - hotkeys!!!
+            - often forgotten as part of UI
+            - [example](https://workflowy.com/)
 * local storage
     * what is persistance?
-    * built in browser storage
-        * saving to localStorage
-        * getting from localStorage
+    * localStorage = built in browser storage
+        * **ONLY SAVES STRINGS**
+        * **IS LOCAL!**
+        * how to save to localStorage
+        * how to get from localStorage
 
 <hr></hr>
 
--- class-13-local_storage-ui_ux/demo/app.js
+++ class-13-local_storage-ui_ux/demo/app.js
@@ -1,30 +1,22 @@
-// TODO save submissions in LocalStorage so I can refresh the page and not lose my list
+var allStudents = [];
 
-// on load get saved submissions and add to the list
-// var submissions = JSON.parse( localStorage.getItem( 'submissions' ) ) || [];
-// if ( submissions ) {
-//     submissions.forEach( function ( submission ) {
-//         addToList( submission );
-//     });
-// }
+function Student ( name, favEle, age ) {
+    this.name = name;
+    this.favEle = favEle;
+    this.age = age;
+}
 
-/* using a separate array
+Student.prototype.yellAge = function () {
+    alert( 'I AM ' + this.age + ' YEARS OLD.' );
+}
 
-    var submissions = [];
-    var savedSubmissions = JSON.parse( localStorage.getItem( 'submissions' ) );
-    if ( savedSubmissions ) {
-        // OPTION 1 set our submissions array to our save submissions data
-        submissions = savedSubmissions; 
+Student.prototype.render = function () {
+    var list = document.querySelector( 'ul' );
+    var newLi = document.createElement( 'li' );
 
-        submissions.forEach( function ( submission ) {
-            addToList( submission );
-            // OPTION 2 add saved submissions to our submissions array and save
-            // saveToLocal( submission );
-        });
-    }
-*/
-
-
+    newLi.innerHTML = this.name + " &#x1F60D;'s " + this.favEle + ' (' + this.age + ')';
+    list.appendChild( newLi );
+}
 
 // handle form submission
 var submissionForm = document.getElementById( 'elementsForm' );
@@ -35,42 +27,14 @@ function submitHandler ( event ) {
 
     var name = this.name.value;
     var htmlEle = this.ele.value;
-    var list = document.querySelector( 'ul' );
-    var newLi = document.createElement( 'li' );
+    var age = this.age.value;
 
-    newLi.innerHTML = name + " &#x1F60D;'s " + htmlEle;
-    list.appendChild( newLi );
+    var student = new Student( name, htmlEle, age );
+    student.render();
+    allStudents.push( student );
 
     this.reset();
 }
 
-// function saveToLocal ( submission ) {
-//     submissions.push( submission );
-//     localStorage.setItem( 'submissions', JSON.stringify( submissions ) );
-// }
-
-
-
-/*
-
-    - show LACK of persistance
-    - introduce LocalStorage - baby database, only stores STRINGS
-        - show scratch tab use of local storage
-        - show localStorage in dev console
-            - update info on screen
-        - stores OBJECTS using JSON
-        - JS Object Notation - looks like an object, but isn't quite the same thing
-            - {"keys":"values"}
-            - everything in strings
-            - we have 2 methods to change js objs -> json string and from json -> obj
-    
-        - in console:
-            - GET data with localStorage.getItem(name)
-            - SET with localStorage.setItem(name, data)
-            - example again using JSON methods
-
-        - okay, now integrate with our form handler!
-        - save data on submit
-
-        - okay, now GET data on load!
-*/
+// TODO save submissions in LocalStorage 
+//      load submissions from LocalStorage and display
